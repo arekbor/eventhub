005f0df4a144d2b6ae901f153768b9edabc2d2c9
arekbor <arekindependent@gmail.com>
2024-06-14 13:49:02 +0200

    clear app component

--- Frontend/eventhub/src/app/app.component.ts
+++ Frontend/eventhub/src/app/app.component.ts
@@ -1,16 +1,7 @@
 import { Component } from "@angular/core";
-import { AuthService } from "@core/services/auth.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
 })
-export class AppComponent {
-  protected isLogged: boolean;
-
-  constructor(private authService: AuthService) {}
-
-  private initUser(): void {
-    this.isLogged = this.authService.isLogged();
-  }
-}
+export class AppComponent {}

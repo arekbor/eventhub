3f36108ddf39deb0cb554add3e5509e28cba9a1e
arekbor <arekindependent@gmail.com>
2024-07-05 12:28:39 +0200

    add miss files to branch

---
+++ Frontend/eventhub/src/app/shared/components/checkbox/checkbox.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, forwardRef } from "@angular/core";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { ControlValueAccessorDirective } from "@shared/directives/control-value-accessor.directive";
+
+@Component({
+  selector: "app-checkbox",
+  templateUrl: "checkbox.component.html",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true,
+    },
+  ],
+})
+export class CheckboxComponent<T> extends ControlValueAccessorDirective<T> {
+  @Input({ required: true }) label: string;
+  @Input() enabled: boolean = true;
+}
---
+++ Frontend/eventhub/src/app/shared/components/date-time-input/date-time-input.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, forwardRef } from "@angular/core";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
+import { ControlValueAccessorDirective } from "@shared/directives/control-value-accessor.directive";
+
+@Component({
+  selector: "app-date-time-input",
+  templateUrl: "date-time-input.component.html",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DateTimeInputComponent),
+      multi: true,
+    },
+  ],
+})
+export class DateTimeInputComponent<
+  T
+> extends ControlValueAccessorDirective<T> {
+  @Input({ required: true }) label: string;
+  @Input({ required: true }) inputId: string;
+  @Input() appendTo: string;
+  @Input() minDate: Date | undefined;
+  @Input() maxDate: Date | undefined;
+  @Input() showTime = true;
+  @Input() isLoading = false;
+}

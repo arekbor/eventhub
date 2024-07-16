88e191dac8b5b013e96162a182a0687d811327e4
arekbor <arekindependent@gmail.com>
2024-07-16 11:11:13 +0200

    refactor updtating calendar permission

--- Backend/Arekbor.EventHub/src/Application/CalendarPermissions/UpdateUserCalendarPermission.cs
+++ Backend/Arekbor.EventHub/src/Application/CalendarPermissions/UpdateUserCalendarPermission.cs
@@ -3,6 +3,7 @@ using Arekbor.EventHub.Application.Common.Exceptions;
 using Arekbor.EventHub.Application.Common.Interfaces;
 using Arekbor.TouchBase.Application.Common.Validators;
 using FluentValidation;
+using Mapster;
 using MediatR;
 
 namespace Arekbor.EventHub.Application.CalendarPermissions;
@@ -32,9 +33,9 @@ internal class UpdateUserCalendarPermissionHandler(
                 ?? throw new NotFoundException($"Calendar permission of user ${currentId} not found");
 
         if(!calendarPermission.CanModify)
-            throw new BadRequestException("Cannot modify this calendar permission");
+            throw new BadRequestException("Cannot modify calendar permission");
         
-        calendarPermission.Access = request.CalendarPermissionBody.Access;
+        calendarPermission = request.CalendarPermissionBody.Adapt(calendarPermission);
 
         await calendarPermissionRepository
             .UpdateOneAsync(calendarPermission, cancellationToken);

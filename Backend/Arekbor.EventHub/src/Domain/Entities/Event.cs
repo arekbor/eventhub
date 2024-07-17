d79fc5d1f43b47be995b1253039520ab7d3fead4
arekbor <arekindependent@gmail.com>
2024-07-17 11:49:51 +0200

    remove User field from Event

--- Backend/Arekbor.EventHub/src/Domain/Entities/Event.cs
+++ Backend/Arekbor.EventHub/src/Domain/Entities/Event.cs
@@ -10,5 +10,4 @@ public class Event : BaseEntity
     public DateTime? End { get; set; }
     public string? Description { get; set; }
     public Guid UserId { get; set; }
-    public User? User { get; set; }
 }
